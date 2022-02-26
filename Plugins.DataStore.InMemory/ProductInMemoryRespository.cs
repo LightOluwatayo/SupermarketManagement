0eab0195d1944a0658bc8317eb930227f1b410ce
LIGHT BLESSING FOREVER <blessbamz@gmail.com>
2022-02-26 17:41:35 -0600

    Worked on aadind Product

---
+++ Plugins.DataStore.InMemory/ProductInMemoryRespository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using CoreBusiness;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class ProductInMemoryRespository : IProductRepository
+    {
+        private List<Product> products;
+        public ProductInMemoryRespository()
+        {
+            //Init with default values 
+            products = new List<Product>()
+            { 
+            
+                new Product { ProductId =1, CategoryId =1, Name = "Iced Tea", Quantity = 100, Price = 1.99},
+                 new Product { ProductId =2, CategoryId =1, Name = "Canada Dry", Quantity = 200, Price = 1.99},
+                 new Product { ProductId =3, CategoryId =2, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50},
+                 new Product { ProductId =3, CategoryId =2, Name = "White Bread", Quantity = 300, Price = 1.50},
+
+            };
+        
+        
+        }
+        void IProductRepository.AddProduct(Product product)
+        {
+            if (products.Any(x => x.Name.Equals(product.Name, StringComparison.OrdinalIgnoreCase))) return;
+
+            if (products != null && products.Count > 0)
+            {
+                var maxId = products.Max(x => x.ProductId);
+                product.ProductId = maxId + 1;
+
+
+            }
+
+            else
+            {
+
+                product.ProductId = 1;
+
+            }
+
+
+
+            products.Add(product);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return products;
+        }
+
+        
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -35,6 +35,7 @@ namespace WebApp
 
             //Dependency Injection for In-Memory Data Store
             services.AddScoped<ICategoryRepository, CategoryInMemoryRepository>();
+            services.AddScoped<IProductRepository, ProductInMemoryRespository>();
 
             //Dependency Injection for Use Cases and Repositories
             services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
@@ -42,6 +43,9 @@ namespace WebApp
             services.AddTransient<IEditCategoryUseCase, EditCategoryUseCase>();
             services.AddTransient<IGetCategoryByIdUseCase, GetCategoryByIdUseCase>();
             services.AddTransient<IDeleteCategoryUseCase, DeleteCategoryUseCase>();
+            services.AddTransient<IViewProductsUseCase, ViewProductsUseCase>();
+            services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
